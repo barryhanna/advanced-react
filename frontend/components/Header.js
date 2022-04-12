7822471b002d4415ee057ddc69666a3491e35fc3
Barry Hanna <barryhanna@users.noreply.github.com>
2022-04-12 21:18:34 +0100

    Initial commit

---
+++ frontend/components/Header.js
@@ -0,0 +1,53 @@
+import Link from 'next/link';
+import styled from 'styled-components';
+import Nav from './Nav';
+
+const Logo = styled.h1`
+  background: red;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-size: 2rem;
+  position: relative;
+  z-index: 2;
+  margin-left: 2rem;
+  transform: skew(-7deg);
+  a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1rem;
+  }
+`;
+
+const HeaderStyles = styled.header`
+  .bar {
+    border-bottom: 10px solid var(--black, black);
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 1px solid var(--black, black);
+  }
+`;
+
+function Header() {
+  return (
+    <HeaderStyles>
+      <div className="bar">
+        <Logo>
+          <Link href="/">Sick fits</Link>
+        </Logo>
+      </div>
+      <div className="sub-bar">
+        <p>Search</p>
+      </div>
+      <Nav />
+    </HeaderStyles>
+  );
+}
+
+export default Header;
---
+++ frontend/pages/sell.js
@@ -0,0 +1,10 @@
+import Page from '../components/Page';
+
+export default function SellPage() {
+  return (
+    <>
+      <p>Sell</p>
+      <p>Sell here</p>
+    </>
+  );
+}
