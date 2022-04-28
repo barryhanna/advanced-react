57988bdcd58ed0579092b6108d979d290c1ac1f9
Barry Hanna <barryhanna@users.noreply.github.com>
2022-04-28 21:05:50 +0100

    Password reset

---
+++ frontend/pages/reset.js
@@ -0,0 +1,20 @@
+import { resetCaches } from 'graphql-tag';
+import RequestReset from '../components/RequestReset';
+import Reset from '../components/Reset';
+
+export default function ResetPage({ query }) {
+  if (!query?.token) {
+    return (
+      <div>
+        <p>Sorry, you must supply token</p>
+        <RequestReset />
+      </div>
+    );
+  }
+  return (
+    <div>
+      <p>Reset your password {query.token}</p>
+      <Reset token={query.token} />
+    </div>
+  );
+}
