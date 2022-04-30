7ca1ef7ab20d458a440595a5b027b0eb49196dda
Barry Hanna <barryhanna@users.noreply.github.com>
2022-04-30 10:05:48 +0100

    Add to cart button

--- backend/.keystone/admin/pages/_app.js
+++ backend/.keystone/admin/pages/_app.js
@@ -198,7 +198,7 @@ export default function App({ Component, pageProps }) {
     <Core>
       <KeystoneProvider
         adminConfig={adminConfig}
-        adminMetaHash="8ezt3h"
+        adminMetaHash="13gakeh"
         fieldViews={fieldViews}
         lazyMetadataQuery={lazyMetadataQuery}
       >
